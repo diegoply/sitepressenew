f69f770bce3ae079900a08fb3edf95ab2122382b
diegoply <ply.dgo@gmail.com>
2026-02-10 11:28:14 +0100

    commit ajout bouton connexion et deconnexion

--- my_project_directory/src/Controller/IndexController.php
+++ my_project_directory/src/Controller/IndexController.php
@@ -5,14 +5,19 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
 
 final class IndexController extends AbstractController
 {
     #[Route('/', name: 'app_index')]
-    public function index(): Response
+    public function index(AuthenticationUtils $authenticationUtils): Response
     {
+        // Récupère le dernier username entré
+        $lastUsername = $authenticationUtils->getLastUsername();
+
         return $this->render('index/index.html.twig', [
             'controller_name' => 'Bienvenue',
+            'last_username' => $lastUsername,
         ]);
     }
 }
